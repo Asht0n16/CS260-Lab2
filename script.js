edfa58b9f72f79a4d1a8eb78ed40a505105d387f
Ashton Hammond <hammosam@byu.edu>
2020-02-14 00:13:00 -0700

    Completed Structure
    I've successfully added the current weather and the forecast with the
    info I want. Now all I have to do is make it look pretty.

---
+++ script.js
@@ -0,0 +1,64 @@
+document.getElementById("weatherSubmit").addEventListener("click", function(event) {
+  event.preventDefault();
+  const value = document.getElementById("weatherInput").value;
+  if (value === "")
+    return;
+  console.log(value);
+  const url = "http://api.openweathermap.org/data/2.5/weather?q=" + value +
+    ",US&units=imperial" + "&APPID=209eedb6880977cc3d05f20385391a66";
+  fetch(url)
+  .then(function(response) {
+    return response.json();
+  }).then(function(json) {
+    //Output results
+    console.log(json);
+    let results = "";
+    results += '<h2 id="weatherHeader">Weather in ' + json.name + "</h2>";
+    results += "<div id='mainWeather'>";
+      for (let i=0; i < json.weather.length; i++) {
+        results += '<img id="mainIcon" src="http://openweathermap.org/img/w/' + json.weather[i].icon + '.png"/>';
+      }
+      results += "<p id='description'>"
+      for (let i=0; i < json.weather.length; i++) {
+        results += json.weather[i].description
+        if (i !== json.weather.length - 1)
+          results += ", "
+      }
+      results += "</p>";
+      results += '<h2 id="temp">' + json.main.temp + " &deg;F</h2>"
+    results += "</div>";
+    results += '<h3>Feels Like: ' + json.main.feels_like + " &deg;F</h3>"
+    results += '<h3>Low: ' + json.main.temp_min + " &deg;F</h3>"
+    results += '<h3>High: ' + json.main.temp_max + " &deg;F</h3>"
+    results += '<h3>Humidity: ' + json.main.humidity + "%</h3>"
+    results += '<h3>Wind Speed: ' + json.wind.speed + " mph</h3>"
+
+    document.getElementById("weatherResults").innerHTML = results;
+  });
+
+  // Forecast
+  const url2 = "http://api.openweathermap.org/data/2.5/forecast?q=" + value +
+    ", US&units=imperial" + "&APPID=209eedb6880977cc3d05f20385391a66";
+    fetch(url2)
+      .then(function(response) {
+        return response.json();
+      }).then(function(json) {
+        // Output Results
+        console.log(json);
+        let forecast = "<h1>5 Day Forecast</h1>";
+        for (let i=0; i < json.list.length / 8; i++) {
+          forecast += "<h2>" + moment(json.list[i * 8].dt_txt).format('MMMM Do, YYYY') + "</h2>";
+          forecast += "<div class='day'>"
+          for (let j=0; j < json.list.length / 5; j++) {
+            forecast += "<div class='timeP'>";
+            forecast += "<h3>" + moment(json.list[(i * 8) + j].dt_txt).format('h:mm a') + "</h3>";
+            forecast += '<img class="foreIcon" src="http://openweathermap.org/img/w/' + json.list[(i * 8) + j].weather[0].icon + '.png"/>'
+            forecast += "<p>" + json.list[(i * 8) + j].weather[0].main + "</p>"
+            forecast += "<p>Temperature: " + json.list[(i * 8) + j].main.temp + "</p>";
+            forecast += "</div>";
+          }
+          forecast += "</div>";
+        }
+        document.getElementById("forecastResults").innerHTML = forecast;
+  });
+});
